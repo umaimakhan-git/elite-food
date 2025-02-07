306353bff85698dc2c40367413e96e46f6ece4f5
umemakhan-git <umemakhan324@gmail.com>
2025-02-07 11:41:51 -0800

    added changes

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import food from './foods'
 import chef from './chefs'
-import blog from './blogs'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [food, chef, blog],
+  types: [food, chef],
 }
