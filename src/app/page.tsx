c8a025a87bb6f2a409b0f181746c06786c1ab41e
umemakhan-git <umemakhan324@gmail.com>
2025-02-09 23:07:57 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import AboutUs from '@/components/homePage/AboutUs';
 import HomePageHero from '@/components/homePage/HomePageHero';
-import IconsBoard from '@/components/homePage/IconsBoard';
+
 import Testimonials from '@/components/homePage/Testimonials';
 import React from 'react';
 
@@ -10,7 +10,7 @@ const Home = () => {
     <main>
       <HomePageHero />
       <AboutUs />
-      <IconsBoard />
+   
       <Testimonials />
     </main>
   )
